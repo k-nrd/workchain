d0815af9afea998be029a81224fd0b0b1419a87d
Gustavo Konrad <g.konrad@outlook.com>
2020-07-31 15:57:45 -0300

    started work on chain

---
+++ blockchain/src/blockchain.rs
@@ -0,0 +1,16 @@
+mod block;
+
+pub struct Blockchain {
+
+}
+
+#[cfg(test)]
+mod blockchain_tests {
+    use super::*;
+    use hex;
+
+    #[test]
+    fn block_identity_test() {
+        unimplemented!();
+    }
+}
